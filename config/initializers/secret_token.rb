73b63988e4cb1737f791947e86b170da375975a0
Brian To <bxt5647@rit.edu>
2013-06-09 23:42:58 -0400

    Make things postgres

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HerokuTest::Application.config.secret_token = 'b920af64e53eeae885f628abe75dbb9c112c65f2e24006be44a66c88874cbf4cf283dbe51f5ef2b7f8a91d734c570061a62f53b989dd67bc25ea2918f61f950a'
